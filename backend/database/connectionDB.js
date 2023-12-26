0e2d9c5e3b129c27c55b0d546a6dff4379166be5
GanikasSa <61113449+MELEK-WEB@users.noreply.github.com>
2023-12-26 17:55:52 +0100

    docker-confs

--- backend/database/connectionDB.js
+++ backend/database/connectionDB.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 const connectDB = () => {
   mongoose.set("strictQuery", false);
+
+
   mongoose
-    .connect(process.env.DB, {
+    .connect(DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
